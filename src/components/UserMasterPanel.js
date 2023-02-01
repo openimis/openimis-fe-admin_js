64d6ebdcb0671969ecf36ef60f28bf5c7513113c
lruzicki <lukasz.ruz@gmail.com>
2023-02-01 11:25:32 +0100

    OP-1090: added language value to component in language picker

--- src/components/UserMasterPanel.js
+++ src/components/UserMasterPanel.js
@@ -156,7 +156,7 @@ const UserMasterPanel = (props) => {
           required
           withNull
           nullLabel={formatMessage("UserMasterPanel.language.null")}
-          value=""
+          value={edited?.language}
           onChange={(language) => onEditedChanged({ ...edited, language })}
         />
       </Grid>
