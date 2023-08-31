ec300fd1f9501b108a47bee641c5270a5c489d1f
wzglinieckisoldevelo <wzgliniecki@soldevelo.com>
2023-08-31 19:35:55 +0200

    ONI-96: Working password generator code.

--- src/helpers/passwordGenerator.js
+++ src/helpers/passwordGenerator.js
@@ -1,20 +1,44 @@
 export const passwordGenerator = (options) => {
+    let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let lowercase = uppercase.toLowerCase();
+    let numbers = '0123456789';
+    let specialCharacters = '!@#$%^&*()_+-=[]{}|;:,.<>?';
     let maximumLength = options?.maximumLength ?? 12;
     let minimumLength = options?.minimumLength ?? 8;
     let length = options?.length ?? 10;
     let isNumberRequired = options?.isNumberRequired ?? false;
-    let isLowerCaseRequired = options?.isLowerCaseRequired ?? false;
+    let isLowerCaseRequired = options?.isLowerCaseRequired ?? true;
     let isUpperCaseRequired = options?.isUpperCaseRequired ?? false;
     let isSpecialSymbolRequired = options?.isSpecialSymbolRequired ?? false;
 
     if( ( minimumLength && !maximumLength ) || ( !minimumLength && maximumLength ) ) {
         length = getRandomInt(minimumLength, maximumLength); // non-constant length would cause password to be harder to guess
       }
-      let seedArray = new Uint32Array(1);
-      let newPassword = self.crypto.getRandomValues(seedArray);
 
 
-      return newPassword
+    let categoryArray = self.crypto.getRandomValues(new Uint32Array(length))
+    categoryArray = Array.from(categoryArray, value => value % 4);
+
+    let password  = "";
+
+    categoryArray.forEach(category =>{
+        if (category == 0){
+            password += uppercase.charAt(Math.floor(Math.random() * uppercase.length));
+        }
+        else if(category == 1){
+            password += lowercase.charAt(Math.floor(Math.random() * lowercase.length));
+        }
+        else if(category == 2){
+            password += specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length));
+        }
+        else{
+            password += numbers.charAt(Math.floor(Math.random() * numbers.length));
+        }
+
+    });
+
+
+    return password
 }
 
 function getRandomInt(min, max) {
