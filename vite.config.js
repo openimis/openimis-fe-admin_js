06b5e75ceb01fdfb52f8ebc750bb678b86f54683
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-07-22 21:39:54 +0530

    package.json

--- vite.config.js
+++ vite.config.js
@@ -48,6 +48,7 @@ export default defineConfig({
         '@emotion/react',    
         '@emotion/styled',
         '@emotion/cache',
+        '@mui/styled-engine',
       ],
       output: {
         globals: {
